105be872868edfcf3fe9ad8e11db583a4785a462
klebermkardel <molinakardel@outlook.com.br>
2026-03-06 06:37:03 -0300

    feat(models): cria model de Categorias

---
+++ backend/src/models/Category.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Category = sequelize.define('Category', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    slug: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    }
+});
+
+module.exports = Category;
