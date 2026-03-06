a1d54252e761351ce5ef407cf0468d6e9252bce6
klebermkardel <molinakardel@outlook.com.br>
2026-03-06 07:12:19 -0300

    fix(config): corrige typo na definição do sequelize e inicia o servidor

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         dialect: 'mysql',
         logging: false,
-        defifne: {
+        define: {
             timestamps: true,
             underscored: true
         }
