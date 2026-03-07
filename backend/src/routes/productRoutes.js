6feaf0250dc1368832d19280b37c74742b5cc6ea
klebermkardel <molinakardel@outlook.com.br>
2026-03-07 17:53:18 -0300

    feat(routes): configura endpoint POST para cadastro de produtos

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -5,5 +5,6 @@ const ProductController = require('../controllers/ProductController');
 // Definindo os caminhos e ligando aos métodos do Controller
 router.get('/', ProductController.getAll);
 router.get('/:id', ProductController.getById);
+router.post('/', ProductController.create);
 
 module.exports = router;
