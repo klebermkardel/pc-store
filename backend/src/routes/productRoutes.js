1bb5b7f4671d8257150021aeb2eb7637ef695c1b
klebermkardel <molinakardel@outlook.com.br>
2026-03-07 10:51:22 -0300

    feat(routes): isola rotas de produtos em arquivo próprio

---
+++ backend/src/routes/productRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const ProductController = require('../controllers/ProductController');
+
+// Definindo os caminhos e ligando aos métodos do Controller
+router.get('/', ProductController.getAll);
+router.get('/:id', ProductController.getById);
+
+module.exports = router;
