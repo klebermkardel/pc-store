46b89f0b260f1ef9d37ec20dc95db34e4a6326b9
klebermkardel <molinakardel@outlook.com.br>
2026-03-07 18:31:26 -0300

    feat(routes): adiciona endpoint PUT para atualização de produtos por ID

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -6,5 +6,6 @@ const ProductController = require('../controllers/ProductController');
 router.get('/', ProductController.getAll);
 router.get('/:id', ProductController.getById);
 router.post('/', ProductController.create);
+router.put('/:id', ProductController.update);
 
 module.exports = router;
