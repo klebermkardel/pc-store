83428eddb5c0a47e79c99255310fde5a463958b1
klebermkardel <molinakardel@outlook.com.br>
2026-03-19 11:59:05 -0300

    fix: atualiza porta de escuta do servidor

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,7 +33,7 @@ const startServer = async () => {
         await sequelize.sync({ force: false });
 
         const PORT = process.env.PORT || 3000;
-        app.listen(PORT, '0.0.0.0', () => {
+        app.listen(PORT, () => {
             console.log(`Servidor rodando na porta ${PORT}`);
         });
 
